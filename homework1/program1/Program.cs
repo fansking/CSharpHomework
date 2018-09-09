7c1402d5508349e32980c8603db318a95d6f9a6a
fansking <935802216@qq.com>
2018-09-09 20:55:11 +0800

    first homework

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double a, b;
+            Console.WriteLine("请输入第一个数字：");
+            str = Console.ReadLine();
+            a = Double.Parse(str);
+            Console.WriteLine("请输入第二个数字：");
+            str = Console.ReadLine();
+            b = Double.Parse(str);
+            Console.WriteLine("这两个数字之积为：" + a * b);
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        string str;
+        double a, b, c;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            str = textBox1.Text;
+            a = Double.Parse(str);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            str = textBox2.Text;
+           b = Double.Parse(str);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = a * b;
+            label3.Text = "两数之积为：" + c;
+           
+        }
+    }
+}
