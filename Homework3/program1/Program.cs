8354d47cf372165aef20d5200467b3f415f72390
fansking <935802216@qq.com>
2018-09-23 21:01:25 +0800

    third

---
+++ Homework3/program1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入您需要的图形(square,circle,rectangle,triangle)：");
+            string input=Console.ReadLine();
+            switch (input) {
+                case "square":
+                    Console.WriteLine("请输入边长：");
+                    double side;
+                    if (double.TryParse(Console.ReadLine(), out side))
+                    {
+                        Square square = new Square(side);
+                        Console.WriteLine("边长为" + side + "的正方形面积为：" + square.Area);
+                    }
+                    else {
+                        Console.WriteLine("输入边长格式有误");
+                    }
+                    break;
+                case "circle":
+                    Console.WriteLine("请输入半径：");
+                    double radius;
+                    if (double.TryParse(Console.ReadLine(), out radius))
+                    {
+                        Circle circle = new Circle(radius);
+                        Console.WriteLine("半径为" +radius + "的圆形面积为：" + circle.Area);
+                    }
+                    else
+                    {
+                        Console.WriteLine("输入边长格式有误");
+                    }
+                    break;
+                case "rectangle":
+                    Console.WriteLine("请输入长：");
+                  
+                    double length,width;
+                    if (!double.TryParse(Console.ReadLine(), out length)) {
+                        Console.WriteLine("长的格式输入有错");
+                        break;
+                    }
+                    Console.WriteLine("请输入宽：");
+                    if (double.TryParse(Console.ReadLine(), out width))
+                    {
+                        Rectangle rectangle = new Rectangle(length, width);
+                        Console.WriteLine("长为"+length+"，宽为"+width+"的长方形面积为：" + rectangle.Area);
+                    }
+                    else
+                    {
+                        Console.WriteLine("输入边长格式有误");
+                    }
+                    break;
+                case "triangle":
+
+                    Console.WriteLine("请输入一个边长：");
+
+                    double _side, height;
+                    if (!double.TryParse(Console.ReadLine(), out _side))
+                    {
+                        Console.WriteLine("边长的格式输入有错");
+                        break;
+                    }
+                    Console.WriteLine("请输入高：");
+                    if (double.TryParse(Console.ReadLine(), out height))
+                    {
+                        Triangle triangle = new Triangle(_side, height);
+                        Console.WriteLine("边长为" + _side + "，高为" + height + "的三角形面积为：" + triangle.Area);
+                    }
+                    else
+                    {
+                        Console.WriteLine("输入边长格式有误");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("输入的图形格式错误或不存在");
+                    break;
+
+            }  
+        }
+    }
+}
---
+++ Homework3/program1/Shape.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    public abstract class Shape
+    {
+        public abstract double Area
+        {
+            get;
+        }
+    }
+
+    public class Square : Shape
+    {
+        private double Side;
+        public Square(double Side)
+        {
+            this.Side = Side;
+        }
+        public override double Area {
+            get
+            {
+                return Side * Side;
+            }
+        } 
+    }
+
+    public class Circle : Shape
+    {
+        private double Radius;
+        public Circle(double Radius) {
+            this.Radius = Radius;
+        }
+        public override double Area  {
+            get {
+                return Math.PI * Radius * Radius;
+            }
+        }
+    }
+    public class Rectangle : Shape {
+        private double Length;
+        private double Width;
+        public Rectangle(double Length, double Width) {
+            this.Length = Length;
+            this.Width = Width;
+        }
+        public override double Area {
+            get {
+                return Length * Width;
+            }
+        }
+    }
+    public class Triangle : Shape {
+        private double Height;
+        private double Side;
+        public Triangle(double Height, double Side) {
+            this.Height = Height;
+            this.Side = Side;
+        }
+        public override double Area
+        {
+            get
+            {
+                return Height*Side/2;
+            }
+        }
+    }
+}
