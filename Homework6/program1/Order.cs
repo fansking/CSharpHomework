0ca34ae880a731980c2d8fed09ff238f6a81b681
fansking <935802216@qq.com>
2018-10-20 23:26:44 +0800

    6

---
+++ Homework6/program1/Order.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    [Serializable]
+    public class Order
+    {
+        public List<OrderDetails> list=new List<OrderDetails>();
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public Order() { }
+        public int TotalValue() {
+            int sum = 0;
+            foreach (OrderDetails details in list)
+            {
+                sum += details.num * details.price;
+            }
+            return sum;
+        }
+        public Order(int id,string name)
+        {
+            this.Id = id;
+            this.ClientName = name;
+        }
+        public void AddOrderDetails(OrderDetails orderDetails)
+        {
+            this.list.Add(orderDetails);
+        }
+        public List<OrderDetails> Getlist()
+        {
+            return list;
+        }
+        public override string ToString()
+        {
+            string str = "订单号：" + Id + "\n客户姓名：" + ClientName + "\n";
+            StringBuilder sb = new StringBuilder(str);
+            int nCount = 1;
+            foreach (OrderDetails orderDetails in list)
+            {
+                sb = sb.Insert(sb.Length, "第" + nCount + "个条目为:\n");
+                nCount++;
+                sb = sb.Insert(sb.Length,orderDetails);
+            }
+            str = sb.ToString();
+            return str;
+
+        }
+
+
+    }
+}
---
+++ Homework6/program1Tests/OrderServiceTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using program2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        OrderService orderService = new OrderService();
+        OrderDetails Orderdetail = new OrderDetails("gun",1,1);
+        Order order = new Order(1,"lv");
+        Order order2 = new Order(2, "yin");
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            orderService.AddOrder(order);
+            Assert.IsNotNull(orderService.GetOrders());
+        }
+
+        [TestMethod()]
+        public void DeleteOrderByIdTest()
+        {
+            orderService.AddOrder(order);
+            orderService.DeleteOrderById(1);
+            Assert.IsTrue((orderService.GetOrders()).Count==0);
+        }
+
+        [TestMethod()]
+        public void updateOrderByIdTest()
+        {
+            orderService.AddOrder(order);
+            orderService.updateOrderById(1, order2);
+            Assert.IsTrue(
+                (orderService.GetOrders())[0].Id == 2
+                );
+        }
+
+        [TestMethod()]
+        public void FindOrderByIdTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.FindOrderById(1);
+        }
+
+        [TestMethod()]
+        public void FindOrderByStuffTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.FindOrderByStuff("gun");
+
+        }
+
+        [TestMethod()]
+        public void FindByClientNameTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.FindByClientName("lv");
+        }
+
+        [TestMethod()]
+        public void FindBigTradeTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.FindBigTrade();           
+        }
+
+
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.Export();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            orderService.AddOrder(order);
+            orderService.AddOrder(order2);
+            orderService.Import();
+        }
+    }
+}
